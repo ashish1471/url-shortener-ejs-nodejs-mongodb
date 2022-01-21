67c72effdc57a0c13ae65f79fd6e478656e74829
Ashish Sharma <ashishsharma@Ashishs-MacBook-Air.local>
2022-01-21 21:20:21 +0530

    first commit

---
+++ models/shortUrl.js
@@ -0,0 +1,24 @@
+const shortId= require('shortid')
+
+const mongoose= require('mongoose')
+const shortUrlSchema=new mongoose.Schema({
+
+    full:{
+        type: String,
+        required:true
+    },
+
+    short:{
+        type: String,
+        required: true,
+        default: shortId.generate
+    },
+
+    clicks:{
+        type: Number,
+        required:true,
+        default:0
+    }
+})
+
+module.exports=mongoose.model('ShortUrl', shortUrlSchema)
